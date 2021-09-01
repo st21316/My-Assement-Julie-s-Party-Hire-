ba57b2103417c3d8b6a4c39ea1e4e020de4d46cd
st21316 <st21316@ormiston.school.nz>
2021-09-02 09:46:48 +1200

    Update Julie’s Party Hire.py
    Creating Entry Box and position it

--- Julie’s Party Hire.py
+++ Julie’s Party Hire.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import Tk
-from tkintet.font import Font
+from tkinter.font import Font
 
 main_window = Tk()
 main_window.title(" Julia's Party Hire ")
@@ -9,16 +9,16 @@ main_window.iconbitmap('E:/Document/DGT/Programming/My Assement/logo.jpg')
 
 fontButton = Font(
     family="Times",
-    size=14,
+    size=10,
     weight="bold",
     slant="roman",
     underline=0,
     overstrike=0)
 
 fontLabel = Font(
-    family="Helvetica",
-    size=16,
-    weight="bold",
+    family="Menlo",
+    size=12,
+    weight="normal",
     slant="roman",
     underline=0,
     overstrike=0)
@@ -27,7 +27,13 @@ fontLabel = Font(
 customer_name_label = Label(main_window, text="Customer Name", font=fontLabel)
 receipt_number_label = Label(main_window, text="Receipt Number", font=fontLabel)
 item_hired_label = Label(main_window, text="Item Hired", font=fontLabel)
-number_hired_label = Label(main_window, text="Number HIred", font=fontLabel)
+number_hired_label = Label(main_window, text="Number Hired", font=fontLabel)
+
+
+customer_name_entry = Entry(main_window, width=50)
+receipt_number_entry = Entry(main_window, width=50)
+item_hired_entry = Entry(main_window, width=50)
+number_hired_entry = Entry(main_window, width=50)
 
 
 customer_name_label.grid(row=2, column=0, ipady=7, ipadx=7)
@@ -36,7 +42,10 @@ item_hired_label.grid(row=4, column=0, ipady=7, ipadx=7)
 number_hired_label.grid(row=5, column=0, ipady=7, ipadx=7)
 
 
-
+customer_name_entry.grid(row=2, column=1, ipady=4, ipadx=4)
+receipt_number_entry.grid(row=3, column=1, ipady=4, ipadx=4)
+item_hired_entry.grid(row=4, column=1, ipady=4, ipadx=4)
+number_hired_entry.grid(row=5, column=1, ipady=4, ipadx=4)
 
 
 
