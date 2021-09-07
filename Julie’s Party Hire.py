72c231ccb1af34ff9c1a9b28fece181c7f1da98e
st21316 <st21316@ormiston.school.nz>
2021-09-07 17:23:37 +1200

    Update Julie’s Party Hire.py
    Respond if user enter all correct info

--- Julie’s Party Hire.py
+++ Julie’s Party Hire.py
@@ -2,48 +2,90 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.font import Font
 
-def delete_one():
-    x = my_tree.selection()[0]
-    my_tree.delete(x)
-count=0
-row=0
-def delete_several():
-    x = my_tree.selection()[0]
-    for record in x:
-        my_tree.delete(record)
 
+count=0
+name_count=0
+    
+#Check to see if information are entered and enter correctly
+def validate():
+    #Check that all Entry Box are enter, set error text if leave blank
+    input_check = 0
+    if len(customer_name_entry.get()) == 0:
+        c_error['text']="Required."
+    else:
+        c_error['text']=""
+        input_check = 1
+        
+    if len(item_hired_entry.get()) == 0:
+        i_error['text']="Required."
+    else:
+        i_error['text']=""
+        input_check = 1
+    
+    #Check that receipt number is enter in number, if not set error text
+    try:
+        int(receipt_number_entry.get())
+        r_error.config(text='')
+    except ValueError:
+        r_error.config(text="Required. Type in number only.")
+        
+    #Check the number of item hired is between 1 and 500, set error text if condition not meet
+    if (receipt_number_entry.get().isdigit()):
+        if int(receipt_number_entry.get()) < 0 :
+            r_error['text']="Please type in number only."
+
+    #Check that number of item hired is enter in number, if not set error text
+    try:
+        int(number_hired_entry.get())
+        n_error.config(text='')
+    except ValueError:
+        n_error.config(text="Required. Type in number between 1-500 only.")     
+        
+    #Check the number of item hired is between 1 and 500, set error text if condition not meet
+    if (number_hired_entry.get().isdigit()):
+        if int(number_hired_entry.get()) < 1 or int(number_hired_entry.get()) > 500:
+            n_error['text']="Please type in number between 1-500 only."
+    #Check if all infromation are enter correctly, if yes tell user to click print
+    if c_error['text']=='' and i_error['text']=='' and r_error['text']=='' and n_error['text']=='':
+        I_correct['text']="All informations are corect. Click 'Print' to update list."        
+
+#Quit subroutine
 def quit():
     main_window.destroy()
 
+#Function for Print button
 def print():
     global count
-    my_tree.insert(parent='', index='end', iid=count, text='',
+    global name_count
+    my_tree.insert(parent='', index='end', iid=count, text=name_count,
                values=(customer_name_entry.get(), receipt_number_entry.get(),
                        number_hired_entry.get(), item_hired_entry.get()))
     count +=1
-    row +=1
+    name_count +=1
     customer_name_entry.delete(0,END)
     receipt_number_entry.delete(0,END)
     number_hired_entry.delete(0,END)
     item_hired_entry.delete(0,END)
+    c_error['text']=""
+    r_error['text']=""
+    n_error['text']=""
+    i_error['text']=""
 
-def validate():
-    input_check = 0
-    if len(customer_name_entry.get()) == 0:
-        c_error['text']="Required"
-        input_check = 1
-    if len(receipt_number_entry.get()) == 0:
-        r_error['text']="Required.   Type in number only"
-        input_check = 1
-    if len(number_hired_entry.get()) == 0:
-        n_error['text']="Required.   Type in number between 1-500 only"
-        input_check = 1
-    if len(item_hired_entry.get()) == 0:
-        i_error['text']="Required"
-        input_check = 1
+#Function for when deleting one item
+def delete_one():
+    x = my_tree.selection()[0]
+    my_tree.delete(x)
+     
+
+#Function for when deleting several item 
+def delete_several():
+    x = my_tree.selection()
+    for record in x:
+        my_tree.delete(record)
+        
 
 
- #Changing btn colour when hover and when leave
+#Changing btn colour when hover and when leave
 def quitColour_hover(e):
     quit['bg']="#555555"
     quit['fg']="White"
@@ -106,6 +148,7 @@ receipt_number_label = Label(main_window, text="Receipt Number:", fg='#020c0a', 
 r_error = Label(main_window, text="", fg='red')
 item_hired_label = Label(main_window, text="Item Hired:", fg='#020c0a', font=fontLabel)
 i_error = Label(main_window, text="", fg='red')
+I_correct = Label(main_window, text="", fg='green')
 number_hired_label = Label(main_window, text="Number Hired:", fg='#020c0a', font=fontLabel)
 n_error = Label(main_window, text="", fg='red')
 
@@ -144,17 +187,15 @@ my_tree = ttk.Treeview(main_window)
 '''Define the column of the Treeview.
 In this case I decide to have 5 column one for each Label and another one is for ROW'''
 #Define the column
-my_tree['columns'] =('Row', 'Customer Name', 'Receipt Number', 'Item Hire', 'Number Hire')
+my_tree['columns'] =('Customer Name', 'Receipt Number', 'Item Hire', 'Number Hire')
 #Formate Our columns
-my_tree.column('#0', width=0, minwidth=0)
-my_tree.column('Row', anchor=W, width=60)
+my_tree.column('#0', width=60, minwidth=0)
 my_tree.column('Customer Name', anchor=CENTER, width=125)
 my_tree.column('Receipt Number', anchor=CENTER, width=105)
 my_tree.column('Item Hire', anchor=CENTER, width=95)
 my_tree.column('Number Hire', anchor=CENTER, width=85)
 #Create Heading
-my_tree.heading('#0', text="", anchor=W)
-my_tree.heading('Row', text="Row No.", anchor=W)
+my_tree.heading('#0', text="Row No.", anchor=W)
 my_tree.heading('Customer Name', text="Customer Name", anchor=CENTER)
 my_tree.heading('Receipt Number', text="Receipt Number", anchor=CENTER)
 my_tree.heading('Item Hire', text="Item Hire", anchor=CENTER)
@@ -173,6 +214,7 @@ number_hired_label.grid(row=4, column=0, ipadx=5, ipady=5, sticky=E)
 c_error.grid(row=2, column=3, ipadx=5, sticky=W)
 r_error.grid(row=3, column=3, ipadx=5, sticky=W)
 i_error.grid(row=5, column=3, ipadx=5, sticky=W)
+I_correct.grid(row=7, column=1, ipadx=5, sticky=W)
 n_error.grid(row=4, column=3, ipadx=5, sticky=W)
 #Position of Entry Box
 customer_name_entry.grid(row=2, column=1, ipady=4, pady=9, ipadx=4, sticky=W)
@@ -180,13 +222,13 @@ receipt_number_entry.grid(row=3, column=1, ipady=4, pady=9, ipadx=4, sticky=W)
 item_hired_entry.grid(row=5, column=1, ipady=4, pady=9, ipadx=4, sticky=W)
 number_hired_entry.grid(row=4, column=1, ipady=4, ipadx=4, sticky=W)
 #Position of Button
-delete_one.grid(row=10, column=3, ipadx=5, ipady=3, padx=10, sticky=W)
-delete_several.grid(row=11, column=3, ipadx=5, ipady=3, padx=10, sticky=W)
+delete_one.grid(row=12, column=3, ipadx=5, ipady=3, padx=10, sticky=W)
+delete_several.grid(row=13, column=3, ipadx=5, ipady=3, padx=10, sticky=W)
 quit.grid(row=0, column=0, ipady=5, pady=10, padx=50, sticky=E)
-print.grid(row=9, column=3, ipadx=5, ipady=2, pady=10, padx=10, sticky=W)
-validate.grid(row=8, column=3, ipadx=5, ipady=2, pady=10, padx=10, sticky=W)
+print.grid(row=11, column=3, ipadx=5, ipady=2, pady=10, padx=10, sticky=W)
+validate.grid(row=10, column=3, ipadx=5, ipady=2, pady=10, padx=10, sticky=W)
 #Position of Treeview
-my_tree.grid(row=7, columnspan=2, rowspan=5, padx=25, pady=10, ipady=5, sticky=W)
+my_tree.grid(row=9, columnspan=2, rowspan=5, padx=25, pady=10, ipady=5, sticky=W)
 
 
 main_window.mainloop()
